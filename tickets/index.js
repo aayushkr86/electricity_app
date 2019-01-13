f0e40d36115999d18d97af86f66c11fc1b89baaa
aayushkr86 <aayushkr10@gmail.com>
2019-01-13 06:05:53 +0530

    node_modu;es gitignored

--- tickets/index.js
+++
@@ -1,7 +0,0 @@
-var ControllerUsers = require('./ControllerTickets')
-var UserQuery = require('./query')
-var request = require('request')
-
-module.exports = function (req, res, next) {
-  ControllerUsers(req, res, next)
-}
